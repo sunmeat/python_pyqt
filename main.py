8580357e495fa10d0bef9362e2fd0be56ce06a2b
Oleksandr Zahoruiko <sunmeatrich@gmail.com>
2026-02-20 12:14:04 +0200

    Add installation comment for PyQt6
    Added a comment for installing PyQt6 using pip.

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import ctypes
 from PyQt6.QtWidgets import QApplication
 from controller import TaskController
 
+# py -m pip install pyqt6
+
 if sys.platform.startswith("win"):
     ctypes.windll.user32.ShowWindow(
         ctypes.windll.kernel32.GetConsoleWindow(), 0
